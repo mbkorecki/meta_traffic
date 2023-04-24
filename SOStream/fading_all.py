ee8cd7c33a86a9f6921015bba9cb82cb36a6d5ff
mbkorecki <mbokorecki@gmail.com>
2023-04-24 20:44:22 +0900

    cleaning the repo

--- SOStream/fading_all.py
+++
@@ -1,7 +0,0 @@
-def fadingAll(M, t_current, var_lambda, fadeThreshold):
-  M_copy = M.copy()
-  for microcluster in M_copy:
-    t = t_current - microcluster['t']
-    fading = microcluster.number_points*2**(-var_lambda*t)
-    if fading < fadeThreshold:
-      M.remove(microcluster)
--- SOStream/new_cluster.py
+++
@@ -1,5 +0,0 @@
-from SOStream.micro_cluster import MicroCluster
-
-def newCluster(vt, ID):
-    return MicroCluster(centroid=vt, data=[vt], ID=ID)
-
